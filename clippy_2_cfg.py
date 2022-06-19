99bad4757b7b2d13bbd445fa7b86d87fe9f0bca2
josiah tan <josiah123t@gmail.com>
2022-06-19 18:55:14 +1000

    committing stuff please work

--- clippy_2_cfg.py
+++ clippy_2_cfg.py
@@ -1,21 +1,69 @@
 # from datetime import datetime
 
+class Config1:
+    def __init__(self, highlighting, file_name):
+        self.highlighting = highlighting
+        self.file_name = file_name
+
+    def initPost(self, obj, clippy):
+        clippy.state.output_file_name = self.file_name
+        clippy.state.efficiency_symbol = "*"
+        clippy.state.max_pad_efficiency = 5
+        clippy.state.max_pad_english = 15
+        if not self.highlighting:
+            clippy.state.colors = {}
+
+        if self.file_name == "clippy.txt":
+            clippy.extremity.start_pre_sources.set()
+            clippy.extremity.stop_pre_sources.set()
+
+    def startPost(self, obj, clippy):
+        if self.file_name == "clippy.txt":
+            clippy.translations.sources.set("Retro")
+            clippy.distillations.sources.set()
+            clippy.formatting.sources.set(
+                    ["Retro", {
+                        "colorscheme": "gruvbox",
+                        "mode": "defaultSuggest"}])
+
+
+config = [
+        Config1(True, "clippy_2.org"),
+        Config1(True, "clippy.txt")
+        ]
+
+# config = [
+#         Config1(True, "clippy_2.org"),
+#         Config1(False, "clippy_2.org")
+#         ]
+
+# class Config:
+#     def __init__(self):
+#         self.file_name = "clippy_2.org"
+#
+#     def initPost(self, obj, clippy):
+#         clippy.state.output_file_name = self.file_name
+#         clippy.state.efficiency_symbol = "*"
+#         clippy.state.max_pad_efficiency = 5
+#         clippy.state.max_pad_english = 15
+#         clippy.state.colors = {}
+#
+# config = Config()
 
 # def initPost(obj, clippy):
-    # clippy.state.output_file_name = "clippy_2.org"
-    # clippy.state.efficiency_symbol = "*"
-    # clippy.state.max_pad_efficiency = 5
-    # clippy.state.max_pad_english = 15
-    # clippy.state.colors = {}
-    # pass
+#     clippy.state.output_file_name = "clippy_2.org"
+#     clippy.state.efficiency_symbol = "*"
+#     clippy.state.max_pad_efficiency = 5
+#     clippy.state.max_pad_english = 15
+#     clippy.state.colors = {}
 
 # def onTranslateSuggest(obj, clippy):
-    # clippy.formatting.org.defaultSuggest(obj, clippy)
-    # clippy.formatting.minimalSuggest(obj, clippy)
-    # clippy.formatting.retro.suggest(obj, clippy)
-    # clippy.formatting.org.debugSuggest(obj, clippy)
-    # clippy.formatting.org.minimalSuggest(obj, clippy)
-    # clippy.formatting.gruvbox.debugSuggest(obj, clippy)
+#     clippy.formatting.org.defaultSuggest(obj, clippy)
+#     clippy.formatting.minimalSuggest(obj, clippy)
+#     clippy.formatting.retro.suggest(obj, clippy)
+#     clippy.formatting.org.debugSuggest(obj, clippy)
+#     clippy.formatting.org.minimalSuggest(obj, clippy)
+#     clippy.formatting.gruvbox.debugSuggest(obj, clippy)
 
 
 # def onStrokedPost(obj, clippy):
